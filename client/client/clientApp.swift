072d11ef364c56587d27622a0071e838fd227d75
jc2409 <andrew092331@gmail.com>
2025-11-02 00:12:01 +0000

    iOS init

---
+++ client/client/clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  clientApp.swift
+//  client
+//
+//  Created by Jaehyung Choi on 01/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
